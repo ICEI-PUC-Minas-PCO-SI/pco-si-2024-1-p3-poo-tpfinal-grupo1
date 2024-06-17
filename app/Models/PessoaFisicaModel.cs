704321cc6cefde252edb728367eeddee0713ec06
ViniciusCelio <viniciusceliofontes@gmail.com>
2024-06-17 01:35:21 -0300

    feat:adiconado Data Annotations para validacao de dados

--- app/Models/PessoaFisicaModel.cs
+++ app/Models/PessoaFisicaModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,14 @@ namespace app.Models
 {
     internal class PessoaFisicaModel : PessoaModel
     {
+        [Required(ErrorMessage = "CPF é necessário", AllowEmptyStrings = false)]
+        [StringLength(11, MinimumLength = 11, ErrorMessage =
+            "CPF deve conter 11 caracteres")]
         public string? CPF { get; set; }
+
+        [Required(ErrorMessage = "Nome é necessário", AllowEmptyStrings = false)]
+        [StringLength(80, MinimumLength = 3, ErrorMessage =
+            "Nome deve conter pelo menos 3 caracteres")]
         public string? Nome { get; set;}
     }
 }
--- app/Models/PessoaModel.cs
+++ app/Models/PessoaModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,16 @@ namespace app.Models
     internal abstract class PessoaModel
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Email é necessário", AllowEmptyStrings = false)]
+        [StringLength(50, MinimumLength =5, ErrorMessage = 
+            "O email deve ter no mínimo 5 caracteres")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Informe um email válido...")]
         public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Nome é necessário", AllowEmptyStrings = false)]
+        [StringLength(11, MinimumLength = 10, ErrorMessage =
+            "Telefone não está correto")]
         public string? Telefone { get; set; }
         public PessoaEnderecoModel? Endereco { get; set; }
     }
